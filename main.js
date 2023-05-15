89a8ebbff94167794a8bfa87d4bd5e4f44621cdf
Raphael Bozoki <raphaelbozoki01@gmail.com>
2023-05-15 13:33:48 +0200

    read login file

--- main.js
+++ main.js
@@ -1,67 +1,80 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    // Wait until page has loaded
-
-    await page.goto('https://www.instagram.com/360_brat/', {
-        waitUntil: 'networkidle0',
-    });
-
-    // POP UP : DECLINER LES COOKIES
-    const button = await page.$x("//button[contains(., 'Decline optional cookies')]");
-    if (button.length > 0) {
-        await button[0].click();
-    } else {
-        throw new Error("Le bouton n'a pas été trouvé");
+fs.readFile('login.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Une erreur est survenue lors de la lecture du fichier', err);
+        return;
     }
+    console.log("data", data);
+    const lines = data.split('\n');
+    const username = lines[0];
+    const password = lines[1];
 
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    const bodyText = await page.evaluate(() => {
-        return Array.from(document.getElementsByTagName('button'))[6]
-    });
 
-    const connectButton = bodyText
-    console.log(connectButton.outerHTML)
-    if (connectButton.length > 0) {
-        await connectButton[0].click();
-    } else {
-        throw new Error("Le bouton n'a pas été trouvé");
+    (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
-    }
+        // Wait until page has loaded
 
-    await new Promise(resolve => setTimeout(resolve, 1000));
-
-    // Wait for log in form
-
-    await Promise.all([
-        page.waitForSelector('[name="username"]'),
-        page.waitForSelector('[name="password"]'),
-        page.waitForSelector('[name="submit"]'),
-    ]);
-
-    // Enter username and password
-
-    await page.type('[name="username"]', 'username');
-    await page.type('[name="password"]', 'password');
-
-    // Submit log in credentials and wait for navigation
-
-    await Promise.all([
-        page.click('[type="submit"]'),
-        page.waitForNavigation({
+        await page.goto('https://www.instagram.com/360_brat/', {
             waitUntil: 'networkidle0',
-        }),
-    ]);
+        });
 
-    // Download PDF
+        // POP UP : DECLINER LES COOKIES
+        const button = await page.$x("//button[contains(., 'Decline optional cookies')]");
+        if (button.length > 0) {
+            await button[0].click();
+        } else {
+            throw new Error("Le bouton n'a pas été trouvé");
+        }
 
-    await page.pdf({
-        path: 'page.pdf',
-        format: 'A4',
-    });
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        const bodyText = await page.evaluate(() => {
+            return Array.from(document.getElementsByTagName('button'))[6]
+        });
 
-    await browser.close();
-})();
+        const connectButton = bodyText
+        console.log(connectButton.outerHTML)
+        if (connectButton.length > 0) {
+            await connectButton[0].click();
+        } else {
+            throw new Error("Le bouton n'a pas été trouvé");
+
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        // Wait for log in form
+
+        await Promise.all([
+            page.waitForSelector('[name="username"]'),
+            page.waitForSelector('[name="password"]'),
+            page.waitForSelector('[name="submit"]'),
+        ]);
+
+        // Enter username and password
+
+        await page.type('[name="username"]', username);
+        await page.type('[name="password"]', password);
+
+        // Submit log in credentials and wait for navigation
+
+        await Promise.all([
+            page.click('[type="submit"]'),
+            page.waitForNavigation({
+                waitUntil: 'networkidle0',
+            }),
+        ]);
+
+        // Download PDF
+
+        await page.pdf({
+            path: 'page.pdf',
+            format: 'A4',
+        });
+
+        await browser.close();
+    })();
+});
